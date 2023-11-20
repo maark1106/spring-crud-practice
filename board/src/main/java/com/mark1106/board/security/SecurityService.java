4a6b02f7da5bb0bb0a8817b12e6b6b1ac0c930ca
maark1106 <mark1106@naver.com>
2023-11-20 22:09:36 +0900

    feat : SecurityService 추가

---
+++ board/src/main/java/com/mark1106/board/security/SecurityService.java
@@ -0,0 +1,43 @@
+package com.mark1106.board.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.security.Key;
+import java.util.Date;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService {
+
+    private static final String SECRET_KEY = "asdjahkjdaskjhasddkjqhkdas";
+
+    public String createToken(String subject, long expTime){
+        if(expTime <= 0){
+            throw new RuntimeException("만료시간이 0보다 커야합니다");
+        }
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signgingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .signWith(signgingKey, signatureAlgorithm)
+                .setExpiration(new Date(System.currentTimeMillis() + expTime))
+                .compact();
+    }
+
+    public String getSubject(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
