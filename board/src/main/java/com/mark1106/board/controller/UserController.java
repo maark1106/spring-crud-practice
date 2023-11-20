c3bdcbffae8f06a4924976c490e0cc6fa71794b3
maark1106 <mark1106@naver.com>
2023-11-20 21:06:17 +0900

    feat : annotation 추가

--- board/src/main/java/com/mark1106/board/controller/UserController.java
+++ board/src/main/java/com/mark1106/board/controller/UserController.java
@@ -4,6 +4,12 @@ import com.mark1106.board.dto.UserDTO;
 import com.mark1106.board.service.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,23 +19,28 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    public UserDTO insertUser(UserDTO user){
+    @PostMapping("")
+    public UserDTO insertUser(@RequestBody UserDTO user){
         return userService.insertUser(user);
     }
 
+    @GetMapping("")
     public List<UserDTO> getAllUsers(){
         return userService.getAllUsers();
     }
 
-    public UserDTO getUserByUserId(String userId){
+    @GetMapping("/{userId}")
+    public UserDTO getUserByUserId(@PathVariable String userId){
         return userService.getUserByUserId(userId);
     }
 
-    public void updateUserPw(String userId, UserDTO user){
+    @PutMapping("/{userId}")
+    public void updateUserPw(@PathVariable String userId,@RequestBody UserDTO user){
         userService.updateUserPw(userId, user);
     }
 
-    public void deleteUser(String userId){
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable String userId){
         userService.deleteUser(userId);
     }
 }
