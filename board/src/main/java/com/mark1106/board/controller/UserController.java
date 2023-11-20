de3dc03cc865d1be387962ed22387a36950d3692
maark1106 <mark1106@naver.com>
2023-11-20 20:43:59 +0900

    feat : userService 구현

--- board/src/main/java/com/mark1106/board/controller/UserController.java
+++ board/src/main/java/com/mark1106/board/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.mark1106.board.controller;
 
+import com.mark1106.board.dto.UserDTO;
+import com.mark1106.board.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+
 }
--- board/src/main/java/com/mark1106/board/service/UserService.java
+++ board/src/main/java/com/mark1106/board/service/UserService.java
@@ -1,6 +1,34 @@
 package com.mark1106.board.service;
 
+import com.mark1106.board.dto.UserDTO;
+import com.mark1106.board.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
+    @Autowired
+    UserRepository userRepository;
 
+    public UserDTO insertUser(UserDTO user){
+        return userRepository.insertUser(user);
+    }
+
+    public List<UserDTO> getAllUsers(){
+        return userRepository.getAllUsers();
+    }
+
+    public UserDTO getUserByUserId(String userId){
+        return userRepository.getUserByUserId(userId);
+    }
+
+    public void updateUserPw(String userId, UserDTO user){
+        userRepository.updateUserPw(userId, user);
+    }
+
+    public void deleteUser(String userId){
+        userRepository.deleteUser(userId);
+    }
 }
