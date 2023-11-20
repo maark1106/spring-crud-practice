2d671cff4f7af10daff2a37f89922fe32f047052
maark1106 <mark1106@naver.com>
2023-11-20 20:48:16 +0900

    feat : userController 구현

--- board/src/main/java/com/mark1106/board/controller/UserController.java
+++ board/src/main/java/com/mark1106/board/controller/UserController.java
@@ -2,6 +2,7 @@ package com.mark1106.board.controller;
 
 import com.mark1106.board.dto.UserDTO;
 import com.mark1106.board.service.UserService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +13,23 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    public UserDTO insertUser(UserDTO user){
+        return userService.insertUser(user);
+    }
 
+    public List<UserDTO> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    public UserDTO getUserByUserId(String userId){
+        return userService.getUserByUserId(userId);
+    }
+
+    public void updateUserPw(String userId, UserDTO user){
+        userService.updateUserPw(userId, user);
+    }
+
+    public void deleteUser(String userId){
+        userService.deleteUser(userId);
+    }
 }
